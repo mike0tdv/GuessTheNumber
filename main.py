a3a2ca527527099d18604f10cf5a30139bf3ed09
mike0tdv <mihailtodev3@gmail.com>
2024-03-16 16:13:41 +0200

    /

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ used_numbers = []
 tries = 0
 success = False
 
+
 # A function to check if the user entered the correct number
 def guessing(guess):
     global used_numbers, tries, success, random_num
 
     if guess == random_num:
         success = True
-        print(f"Congrats! You guessed the number - {random_num} with {tries} tries!")
     if guess is not random_num:
         used_numbers.append(guess)
+        tries += 1
+
 
 
 # Choosing a range for the random number to be chosen from
@@ -31,4 +33,8 @@ random_num = random.randint(num1, num2)
 while success == False:
     guess = int(input("Enter your guess: "))
     guessing(guess)
-
+    print("The numbers you have used: ")
+    for num in used_numbers:
+        print(num, end=", ")
+    print()
+else: print(f"Congrats! You guessed the number - {random_num} with {tries} tries!")
